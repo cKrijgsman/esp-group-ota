1a44546b05413473e44b81e6c4019505b436a7ca
terrywestmaas <terry@west-maas.nl>
2021-02-10 01:14:06 +0100

    kinda works. nice ;)

--- ESP templates/LOLIND32-ESP32-standard_node/LOLIND32-ESP32-standard_node.ino
+++ ESP templates/LOLIND32-ESP32-standard_node/LOLIND32-ESP32-standard_node.ino
@@ -4,27 +4,160 @@
 #include <AsyncUDP.h> //sending/receiving UDP packets (commands)
 #include <ArduinoOTA.h> // only for InternalStorage
 
+#define SCRIPT_VERSION "1"
 //network setup
 #define SECRET_SSID   "Ziggo9794608"
 #define SECRET_PASS   "r76Jfaxnhtje"
 //server setup
-#define SERVER_ID     "www.my-hostname.it"   // Set your correct hostname
-//#define SERVER_PATH   "/update-v%d.bin"     // Set the URI to the .bin firmware
+#define SERVER_ID     "www.my-hostname.it"  // Set your correct hostname
+#define SERVER_PATH   "/file/%s"            // Set the URI to the .bin firmware
 #define SERVER_PORT   80                    // Commonly 80 (HTTP) | 443 (HTTPS)
+#define UDP_BROADCAST_PORT  41234           // broadcast port number
+#define UDP_LISNEN_PORT     41222           // command receiving port
 
-
-const short VERSION = 1;
+char *NODE_NAME = "berta";
+char *NODE_GROUP = "koeien";
+char *NODE_VERSION = SCRIPT_VERSION;
 
 const char MY_SSID[] = SECRET_SSID;
 const char MY_PASS[] = SECRET_PASS;
 
-WiFiClient    wifiClient;  // HTTP
+bool ChekWiFiEnable = false;
+
+//creating a timer
+hw_timer_t *timer0 = NULL;
+//hw_timer_t * timer1 = NULL;
+
+WiFiClient wifiClient;  // HTTP
 int status = WL_IDLE_STATUS;
+AsyncUDP udp;
+
+//TimerChekWiFi interrupt
+void IRAM_ATTR TimerChekWiFi() {
+    ChekWiFiEnable = true;
+}
+
+void setup() {
+
+    Serial.begin(115200);
+    while (!Serial);
+
+    Serial.print("Sketch version ");
+    Serial.println(NODE_VERSION);
+
+    //config TimerChekWiFi//
+    /* Use 1st timer of 4 */
+    /* 1 tick takes 1/(80MHZ/40000) = 500us so we set divider 40000 and count up */
+    timer0 = timerBegin(0, 40000, true);
+
+    /* Attach TimerChekWiFi function to timer0 */
+    timerAttachInterrupt(timer0, &TimerChekWiFi, true);
+
+    /* Set alarm to call onTimer function every 5 min 1 tick is 500us
+    => 1 second is 2000us */
+    /* Repeat the alarm (third parameter) */
+    timerAlarmWrite(timer0, 10000, true);
+
+    /* Start an alarm */
+    timerAlarmEnable(timer0);
+    Serial.println("start TimerChekWiFi");
+
+
+
+    //------------------------------------------ non OTA setup code --------------------------
+
+}
+
+void loop() {
+
+    if (ChekWiFiEnable == true) {
+        if ((WiFi.status() != WL_CONNECTED) && (CheckWiFiPossibility() == true)) {
+            ConnectToWiFi();
+            //timerAlarmEnable(timer1);
+            if(udp.listen(1234)) {
+                Serial.print("UDP Listening on IP: ");
+                Serial.println(WiFi.localIP());
+                udp.onPacket([](AsyncUDPPacket packet) {
+                    Serial.print("UDP Packet Type: ");
+                    Serial.print(packet.isBroadcast()?"Broadcast":packet.isMulticast()?"Multicast":"Unicast");
+                    Serial.print(", From: ");
+                    Serial.print(packet.remoteIP());
+                    Serial.print(":");
+                    Serial.print(packet.remotePort());
+                    Serial.print(", To: ");
+                    Serial.print(packet.localIP());
+                    Serial.print(":");
+                    Serial.print(packet.localPort());
+                    Serial.print(", Length: ");
+                    Serial.print(packet.length());
+                    Serial.print(", Data: ");
+                    Serial.write(packet.data(), packet.length());
+                    Serial.println();
+                    //reply to the client
+                    packet.printf("Got %u bytes of data", packet.length());
+                });
+            }
+
+            broadcastToServer();
+        }
+    }
+    Serial.print(" \n . ");
+    delay(1000);
+    //handleSketchDownload();
+    //broadcastToServer();
+
+
+    // add your normal loop code below ...
+}
+
+void onPacketCallBack(AsyncUDPPacket packet) {
+
+    Serial.print("UDP Packet Type: ");
+    Serial.print(packet.isBroadcast() ? "Broadcast" : packet.isMulticast() ? "Multicast" : "Unicast");
+    Serial.print(", From: ");
+    Serial.print(packet.remoteIP());
+    Serial.print(":");
+    Serial.print(packet.remotePort());
+    Serial.print(", To: ");
+    Serial.print(packet.localIP());
+    Serial.print(":");
+    Serial.print(packet.localPort());
+    Serial.print(", Length: ");
+    Serial.print(packet.length());
+    Serial.print(", Data: ");
+    Serial.write(packet.data(), packet.length());
+    packet.flush();
+
+
+}
+
+void broadcastToServer() {
+    byte mac[6];
+    WiFi.macAddress(mac);
+    // print MAC address
+    /*Serial.print("MAC: ");
+    Serial.print(mac[5], HEX);
+    Serial.print(":");
+    Serial.print(mac[4], HEX);
+    Serial.print(":");
+    Serial.print(mac[3], HEX);
+    Serial.print(":");
+    Serial.print(mac[2], HEX);
+    Serial.print(":");
+    Serial.print(mac[1], HEX);
+    Serial.print(":");
+    Serial.println(mac[0], HEX);*/
+    //udp broadcast on port
+    String builtString =
+            String(mac[5]) + ":" + String(mac[4]) + ":" + String(mac[3]) + ":" + String(mac[2]) + ":" + String(mac[1]) +
+            ":" + String(mac[0]) + "|" + String(NODE_NAME) + "|" + String(NODE_GROUP) + "|" + String(NODE_VERSION);
+    udp.broadcastTo(builtString.c_str(), UDP_BROADCAST_PORT);
+}
 
 void handleSketchDownload() {
-    const char* SERVER = SERVER_ID;  // hostname
+    const char *SERVER = SERVER_ID;  // hostname
     const unsigned short PORT = SERVER_PORT; // Port number
-    const char* PATH = "/update-v%d.bin";       // URI to the .bin firmware
+    const char *PATH = SERVER_PATH;       // URI to the .bin firmware
     const unsigned long CHECK_INTERVAL = 6000;  // Time interval between update checks (ms)
 
     // Time interval check
@@ -37,7 +170,7 @@ void handleSketchDownload() {
     HttpClient client(wifiClient, SERVER, PORT);  // HTTP
 
     char buff[32];
-    snprintf(buff, sizeof(buff), PATH, VERSION + 1);
+    snprintf(buff, sizeof(buff), PATH);
 
     Serial.print("Check for update file ");
     Serial.println(buff);
@@ -91,36 +224,47 @@ void handleSketchDownload() {
     InternalStorage.apply(); // this doesn't return
 }
 
-void setup() {
+bool CheckWiFiPossibility() {
+    int n = WiFi.scanNetworks(1);
+    Serial.println("scan done");
+    if (n == 0) {
+        Serial.println("no networks found");
+    } else {
+        Serial.print(n);
+        Serial.println(" networks found");
+        for (int i = 0; i < n; ++i) {
+            // Print SSID and RSSI for each network found
+            if (WiFi.SSID(i) == MY_SSID) { //enter the ssid which you want to search
+                Serial.println("The network you are looking for is available");
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    Serial.println("");
 
-    Serial.begin(115200);
-    while (!Serial);
+}
 
-    Serial.print("Sketch version ");
-    Serial.println(VERSION);
-
+bool ConnectToWiFi() {
+    int i = 0;
     Serial.println("Initialize WiFi");
+    WiFi.mode(WIFI_STA);
     WiFi.begin(MY_SSID, MY_PASS);
 
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
+        i++;
+        if (i == 5) {
+            return false;
+        }
     }
 
     Serial.println("");
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
-    //------------------------------------------ non OTA setup code --------------------------
-
-    String builtString = String("victor|Birds|1");
-
-    udp.broadcastTo(builtString.c_str(), 41234);
+    return true;
 }
 
-void loop() {
-    // check for updates
-    //handleSketchDownload();
-
-    // add your normal loop code below ...
-}
